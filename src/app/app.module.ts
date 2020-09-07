d8d11e714690cddd3edc098a9a6f1641bdfb5aab
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-09-07 20:24:45 -0300

    Screen completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,8 @@ import { HeaderComponent } from './components/header/header.component';
     MatIconModule,
     MatInputModule,
     MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
