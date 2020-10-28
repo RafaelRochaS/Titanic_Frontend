7a1a5c9479709c254e5b35473a3be7583f33ea93
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-10-28 19:23:40 -0300

    Remove security audit for now

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
 
 describe('DataService', () => {
   let service: DataService;
